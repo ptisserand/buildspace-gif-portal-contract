3cd66f4bfdca2026f9d1e29667b74522e16f4ae6
Patrice Tisserand <p.tisserand@gmail.com>
2022-01-30 14:25:25 +0100

    Initial import

---
+++ programs/myepicproject/src/lib.rs
@@ -0,0 +1,42 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        // get a reference to account
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs +=1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer=user, space=9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// specify which data we want for add_gif
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
