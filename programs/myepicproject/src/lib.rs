5cd1f2003ee4851b39aa95702b55cdf53d34e519
Patrice Tisserand <p.tisserand@gmail.com>
2022-01-30 15:25:09 +0100

    Updated for devnet deploy

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Dt8bpxueHcsn35VdN1EoYgFCaS2tJcP7YodZyMMby6Se");
 
 #[program]
 pub mod myepicproject {
